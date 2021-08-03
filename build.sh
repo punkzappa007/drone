57b9693fddeeb78487e62b9c132058beba973575
MasterSenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-08-03 21:08:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,13 +25,13 @@ cd ~/OrangeFox_10/fox_10.0
 git clone --depth=1 $DT_LINK $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
+. build/envsetup.sh
+echo " source build/envsetup.sh done"
 export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 export LC_ALL="C"
-. build/envsetup.sh
-echo " source build/envsetup.sh done"
 lunch omni_${DEVICE}-eng || abort " lunch failed with exit status $?"
-echo " lunch omni_$DEVICE-eng done"
+echo " lunch omni_${DEVICE}-eng done"
 mka recoveryimage || abort " mka failed with exit status $?"
 echo " mka recoveryimage done"
 
