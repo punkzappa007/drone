dc6075384b26d06dc6302a4fcac8fa100650cf5a
MasterSenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-08-03 23:05:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,26 +2,25 @@
 
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="https://gitlab.com/OrangeFox/sync.git"
+#MANIFEST="https://gitlab.com/OrangeFox/sync.git"
 DEVICE=CD6
 DT_LINK="https://github.com/mastersenpai05/twrp_device_TECNO_CD6.git"
 DT_PATH=device/TECNO/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
-git clone https://gitlab.com/OrangeFox/misc/scripts ~/
+mkdir ~/OrangeFox_10 && cd ~/OrangeFox_10
+git clone https://gitlab.com/OrangeFox/misc/scripts
 cd ~/scripts
 sudo bash setup/android_build_env.sh
 sudo bash setup/install_android_sdk.sh
 
 echo " ===+++ Syncing Recovery Sources +++==="
-mkdir ~/OrangeFox_10
-cd ~/OrangeFox_10
 git clone https://gitlab.com/OrangeFox/sync.git
 cd sync
 ./get_fox_10.sh ~/OrangeFox_10/fox_10.0
 cd ~/OrangeFox_10/fox_10.0
-git clone -b orangefox $DT_LINK $DT_PATH
+git clone --depth=1 $DT_LINK $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
 . build/envsetup.sh
@@ -38,5 +37,5 @@ echo " mka recoveryimage done"
 echo " ===+++ Uploading Recovery +++==="
 cd out/target/product/$DEVICE
 ofoxzip="$(ls *.zip)"
-curl -T $ofoxzip https://git.io/file-transfer | sh
+curl -sL $ofoxzip https://git.io/file-transfer | sh
 ./transfer wet *.zip
