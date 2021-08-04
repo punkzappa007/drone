e12a9d158139041b49327598dfa6b5a7a395abac
MasterSenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-08-05 03:02:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-
 # Just a basic script U can improvise lateron asper ur need xD 
 
-#MANIFEST="https://gitlab.com/OrangeFox/sync.git"
+MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0"
 DEVICE=CD6
 DT_LINK="https://github.com/mastersenpai05/twrp_device_TECNO_CD6 -b orangefox"
 DT_PATH=device/TECNO/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
+mkdir -p ~/OrangeFox_10
+cd ~/OrangeFox_10
 apt install openssh-server -y
-mkdir ~/OrangeFox_10 && cd ~/OrangeFox_10
 git clone https://gitlab.com/OrangeFox/misc/scripts
-cd ~/scripts
+cd scripts
 sudo bash setup/android_build_env.sh
 sudo bash setup/install_android_sdk.sh
 
 echo " ===+++ Syncing Recovery Sources +++==="
+cd ~/OrangeFox_10
 git clone https://gitlab.com/OrangeFox/sync.git
 cd sync
 ./get_fox_10.sh ~/OrangeFox_10/fox_10.0
@@ -23,9 +24,16 @@ cd ~/OrangeFox_10/fox_10.0
 git clone --depth=1 $DT_LINK $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
- source build/envsetup.sh
-export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+rm -rf out
+source build/envsetup.sh
+
+version=$(cat bootable/recovery/variables.h | grep "define FOX_MAIN_VERSION_STR" | cut -d \" -f2)
+wget -O ~/OrangeFox_10/Magisk.zip https://github.com/topjohnwu/Magisk/releases/download/v23.0/Magisk-v23.0.apk
+export FOX_VERSION="${version}_0"
+export FOX_USE_SPECIFIC_MAGISK_ZIP="$HOME/OrangeFox_10/Magisk.zip"
+
+export ALLOW_MISSING_DEPENDENCIES=true 
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1 
 export LC_ALL="C"
 lunch omni_${DEVICE}-eng && mka recoveryimage
 
