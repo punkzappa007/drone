8328f7cb299917562278d73f55bb48ea9e4c1fe1
MasterSenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-08-06 05:15:44 +0800

    TECNO CD6

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 # Just a basic script U can improvise lateron asper ur need xD 
 
 MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni -b twrp-10.0"
-DEVICE=RMX2121
-DT_LINK="https://github.com/mastersenpai05/twrp_device_realme_rmx2121"
-DT_PATH=device/realme/$DEVICE
+DEVICE=CD6
+DT_LINK="https://github.com/mastersenpai05/twrp_device_TECNO_CD6"
+DT_PATH=device/TECNO/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
