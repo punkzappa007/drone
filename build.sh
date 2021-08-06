6d0ba00a0741e454af7f9c0da901031e22841329
MasterSenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-08-06 20:10:23 +0800

    okay

--- build.sh
+++ build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="https://gitlab.com/OrangeFox/sync.git"
+MANIFEST="git clone git@gitlab.com:OrangeFox/sync.git"
 DEVICE=CD6
 DT_LINK="https://github.com/mastersenpai05/twrp_device_TECNO_CD6 -b orangefox"
 DT_PATH=device/TECNO/$DEVICE
-
-USE_SSH="0";
 
 echo " ===+++ Setting up Build Environment +++==="
 cd ~ && git clone https://github.com/akhilnarang/scripts && bash scripts/setup/android_build_env.sh
 
 echo " ===+++ Syncing Recovery Sources +++==="
+pwd
 mkdir ~/OrangeFox_10
 cd ~/OrangeFox_10
 git clone $MANIFEST
@@ -25,9 +24,7 @@ rm -rf out
 source build/envsetup.sh
 
 version=$(cat bootable/recovery/variables.h | grep "define FOX_MAIN_VERSION_STR" | cut -d \" -f2)
-wget -O ~/OrangeFox_10/Magisk.zip https://github.com/topjohnwu/Magisk/releases/download/v23.0/Magisk-v23.0.apk
 export FOX_VERSION="${version}_0"
-export FOX_USE_SPECIFIC_MAGISK_ZIP="$HOME/OrangeFox_10/Magisk.zip"
 export ALLOW_MISSING_DEPENDENCIES=true 
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1 
 export LC_ALL="C"
