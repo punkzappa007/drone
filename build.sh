ae648a2b0d4b8172869e367c553ca65fa212cb6e
MasterSenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-08-06 12:04:50 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,12 +11,12 @@ echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
 apt update --fix-missing
 apt install openssh-server -y
+mkdir -p cam && cd ~/cam
 
 echo " ===+++ Syncing Recovery Sources +++==="
-cd ~/
 git clone --depth=1 $DT_LINK
 
 echo " ===+++ Building Recovery +++==="
-cd ~/AIK-Linux
+cd ~/cam/AIK-Linux
 wget https://dumps.tadiphone.dev/dumps/poco/aresin/-/raw/ares-user-11-RP1A.200720.011-V12.5.3.0.RKJCNXM-release-keys/boot.img
-./repackimg.sh boot.img
+./unpackimg.sh boot.img
