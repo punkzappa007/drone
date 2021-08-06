c7752ae15d99b6cd1f06875a8dcf5b72c3ba941b
MasterSenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-08-06 11:49:01 +0800

    Aik

--- build.sh
+++ build.sh
@@ -2,37 +2,21 @@
 
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni -b twrp-10.0"
-DEVICE=RMX2121
-DT_LINK="https://github.com/mastersenpai05/twrp_device_realme_rmx2121"
-DT_PATH=device/realme/$DEVICE
+#MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni -b twrp-10.0"
+#DEVICE=RMX2121
+DT_LINK="https://github.com/draekko/AIK-Linux.git"
+#DT_PATH=device/realme/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
 apt update --fix-missing
 apt install openssh-server -y
-mkdir ~/twrp10 && cd ~/twrp10
 
 echo " ===+++ Syncing Recovery Sources +++==="
-repo init --depth=1 -u $MANIFEST
-repo sync
-git clone --depth=1 $DT_LINK $DT_PATH
+cd ~/
+git clone --depth=1 $DT_LINK
 
 echo " ===+++ Building Recovery +++==="
-. build/envsetup.sh
-export TW_THEME=portrait_hdpi
-export ALLOW_MISSING_DEPENDENCIES=true
-lunch omni_${DEVICE}-eng && mka recoveryimage
-
-# Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
-echo " ===+++ Uploading Recovery +++==="
-version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
-OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
-
-cd out/target/product/$DEVICE
-mv recovery.img ${OUTFILE%.zip}.img
-zip -r9 $OUTFILE ${OUTFILE%.zip}.img
-
-#curl -T $OUTFILE https://oshi.at
-curl -sL $OUTFILE https://git.io/file-transfer | sh
-./transfer wet *.zip
+cd ~/AIK-Linux
+wget https://dumps.tadiphone.dev/dumps/poco/aresin/-/raw/ares-user-11-RP1A.200720.011-V12.5.3.0.RKJCNXM-release-keys/boot.img
+./repackimg.sh boot.img
