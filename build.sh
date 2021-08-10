4b29582ba7733586e0413c40fff2e0f4dc98f36a
MasterSenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-08-10 16:23:52 +0800

    Juice

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 # Just a basic script U can improvise lateron asper ur need xD 
 
 MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
-DEVICE=ares 
-DT_LINK="https://github.com/mastersenpai05/twrp_device_xiaomi_ares"
+DEVICE=juice
+DT_LINK="https://github.com/mastersenpai05/twrp_device_xiaomi_juice -b android-11.0"
 DT_PATH=device/xiaomi/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
@@ -20,7 +20,6 @@ git clone --depth=1 $DT_LINK $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
 . build/envsetup.sh
-export TW_THEME=portrait_hdpi
 export ALLOW_MISSING_DEPENDENCIES=true
 lunch twrp_${DEVICE}-eng && mka recoveryimage
 
